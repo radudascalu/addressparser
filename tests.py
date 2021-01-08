c1d8e5c4d3fd5d9514438eae4c964f38409150a2
radudascalu <raduciprian.dascalu@gmail.com>
2021-01-08 18:36:07 +0100

    test: add advanced test cases

--- tests.py
+++ tests.py
@@ -3,14 +3,21 @@ from app import parse_address
 import unittest
 
 class TestParseAddress(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestParseAddress, self).__init__(*args, **kwargs)
+        self.test_cases = pd.read_csv("testcases.csv") 
 
-    def test_parse_address(self):
-        testcases = pd.read_csv("testcases.csv") 
-        
-        for idx, test_row in testcases.iterrows():
+    def run_testcases(self, difficulty):
+        for idx, test_row in self.test_cases[self.test_cases.difficulty == difficulty].iterrows():
             parsed = parse_address(test_row['address'])
             self.assertEqual(parsed['street'], test_row['street'])
             self.assertEqual(parsed['housenumber'], test_row['housenumber'])
+
+    def test_simple(self):
+        self.run_testcases(1)
+        
+    def test_advanced(self):
+        self.run_testcases(2)
 
 if __name__ == "__main__":
     unittest.main()
